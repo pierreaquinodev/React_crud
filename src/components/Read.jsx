f5d63e1c3e3f51dc1027f45d0957b23a2cd422bb
pierreaquinodev <pierre_aquino@hotmail.com>
2022-11-06 19:12:59 -0300

    edit feature commit

--- src/components/Read.jsx
+++ src/components/Read.jsx
@@ -1,10 +1,14 @@
 /* eslint-disable */
-
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import "./Read.css";
 
 const Read = () => {
+    const firstName = useRef(null);
+    const lastName = useRef(null);
+    const age = useRef(null);
+    const email = useRef(null);
+
     const [person, setPerson] = useState([]);
 
     useEffect(() => {
@@ -30,10 +34,41 @@ const Read = () => {
         window.location.reload();
     }
 
-    function editPerson() {}
+    function editPerson(id) {
+        async function getEditPerson() {
+            const getPerson = await axios
+                .get(`http://localhost:3000/person/${id.target.id}`)
+                .then((res) => {
+                    firstName.current.value = res.data.first_name;
+                    lastName.current.value = res.data.last_name;
+                    age.current.value = res.data.age;
+                    email.current.value = res.data.email;
+                })
+                .catch((err) => {
+                    console.log(`Ocorreu um erro: ${err}`);
+                });
+        }
+        getEditPerson();
+    }
 
     return (
         <div className="readContainer">
+            <form className="editForm" action="">
+                <label>first name</label>
+                <input type="text" ref={firstName} />
+                <br />
+                <label>last name</label>
+                <input type="text" ref={lastName} />
+                <br />
+                <label>age</label>
+                <input type="text" ref={age} />
+                <br />
+                <label>email</label>
+                <input type="text" ref={email} />
+                <br />
+                <button>Save</button>
+            </form>
+
             <table className="personTable">
                 <thead>
                     <tr>
@@ -55,21 +90,17 @@ const Read = () => {
                                 <td>{eachPerson.age}</td>
                                 <td>{eachPerson.email}</td>
                                 <th>
-                                    <button
-                                        onClick={deletePerson}
-                                        className="deleteButton"
-                                    >
+                                    <button className="deleteButton">
                                         <img
+                                            onClick={deletePerson}
                                             id={eachPerson.id}
                                             src="/delete.png"
                                             width="20px"
                                         />
                                     </button>
-                                    <button
-                                        onClick={editPerson}
-                                        className="editButton"
-                                    >
+                                    <button className="editButton">
                                         <img
+                                            onClick={editPerson}
                                             id={eachPerson.id}
                                             src="/edit.png"
                                             width="20px"
